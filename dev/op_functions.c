e06470298a875078a70d77651ffde3d9feda470c
leobyeon <386@holbertonschool.com>
2018-07-29 19:38:06 +0000

    Revised the functions to match printf.c

--- dev/op_functions.c
+++ dev/op_functions.c
@@ -1,60 +1,95 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdarg.h>
 
-char op_char(char *str, char *op, void *val)
+/**
+  * op_char - takes in an argument and checks for a null terminating byte
+  * @str: original string
+  * @args: character passed
+  * Return: pointer to str
+  */
+char *op_char(char *str, va_list args)
 {
+	char *newstr = va_arg(args, char *);
+
+	return (newstr);
+}
+
+
+/**
+  * op_str - takes in an argument and checks for a null terminating byte
+  * @str: original string
+  * @args: character passed
+  * Return: pointer to str
+  */
+char *op_str(char *str, va_list args)
+{
+	char *newstr = va_arg(args, char *);
+
+	return (newstr);
+}
+
+
+/**
+  * op_int - takes in an argument and converts int to char
+  * @str: original string
+  * @args: character passed
+  * Return: pointer to str
+  */
+char *op_int(char *str, va_list args)
+{
+	unsigned int val_len;
 	unsigned int old_len;
-	unsigned int new_size;
+	int val = va_arg(args, int);
 	char *newstr;
-	val = (int *)val;
 
-	for (old_len = 0; str[old_len]; old_len++)
-		;
+	if (val >= 0)
+	{
+		val_len = (val >= 1000000000) ? 10 : (val >= 100000000) ? 9 : 
+			(val >= 10000000) ? 8 : (val >= 1000000) ? 7 : 
+			(val >= 100000) ? 6 : (val >= 10000) ? 5 : 
+			(val >= 1000) ? 4 : (val >= 100) ? 3 : 
+			(val >= 10) ? 2 : 1;
 
-	new_size = (old_len + 1) + 1;
+		newstr = malloc(val_len + 1);
+		if (!newstr)
+			return (NULL);
+	}
 
-	newstr = _realloc(str, old_len, new_size);
+	else
+	{
+		val_len = (val <= -1000000000) ? 10 : (val <= -100000000) ? 9 : 
+			(val <= 10000000) ? 8 : (val <= -1000000) ? 7 : 
+			(val <= -100000) ? 6 : (val <= -10000) ? 5 : 
+			(val <= -1000) ? 4 : (val <= -100) ? 3 : 
+			(val <= -10) ? 2 : 1;
 	
-	newstr[old_len++] = val;
-	newstr[old_len++] = '\0';
+		newstr = malloc(val_len + 2);
+		if (!newstr)
+			return (NULL);
+	}
+
+	itoa(val, newstr);
+
+	newstr[val_len + 1] = '\0';
 
 	return (newstr);
 }
 
-char op_str(char *str, char *op, void *val)
-{
-	unsigned int old_len;
-	unsigned int val_len;
-	unsigned int new_size;
-	char *newstr;
-	val = (int *)val;
 
-	for (old_len = 0; str[old_len]; old_len++)
-		;
-
-	for (val_len = 0; val[val_len]; val_len++)
-		;
-
-	new_size = (old_len + 1) + val_len;
-
-	newstr = _realloc(str, old_len, new_size);
-
-	for (val_len = 0; val[val_len]; val_len++)
-		newstr[old_len++] = val[val_len];
-
-	newstr[old_len++] = '\0';
-
-	return (newstr);
-}
-
-char op_int(char *str, char *op, void *val)
+/**
+  * op_dec - takes in an argument and converts int to char
+  * @str: original string
+  * @args: character passed
+  * Return: pointer to str
+  */
+char *op_dec(char *str, va_list args)
 {
 	unsigned int old_len;
 	unsigned int val_len;
 	unsigned int new_size;
 	unsigned int i = 0;
 	char *newstr;
-	val = (char *)val;
 
 	for (old_len = 0; str[old_len]; old_len++)
 		;
@@ -73,31 +108,3 @@ char op_int(char *str, char *op, void *val)
 
 	return (newstr);
 }
-
-char op_dec(char *str, char *op, void *val)
-{
-	unsigned int old_len;
-	unsigned int val_len;
-	unsigned int new_size;
-	unsigned int i = 0;
-	char *newstr;
-	val = (char *)val;
-
-	for (old_len = 0; str[old_len]; old_len++)
-		;
-
-	val_len = (val >= 1000000000) ? 9 : (val >= 100000000) ? 8 : 
-		(val >= 10000000) ? 7 : (val >= 1000000) ? 6 : 
-		(val >= 100000) ? 5 : (val >= 10000) ? 4 : 
-		(val >= 1000) ? 3 : (val >= 100) ? 2 : 
-		(val >= 10) ? 1 : 0;
-
-	new_size = (old_len + 1) + val_len;
-
-	newstr = _realloc(str, old_len, new_size);
-
-	itoa(val, newstr);
-
-	return (newstr);
-}
-
