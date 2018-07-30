1689ae4825fb29dd6e33b25556cfd052aa1b7f5e
Pete Wu-Vagrant <pete.h.wu@gmail.com>
2018-07-30 13:31:52 -0700

    fixed betty issues

--- helper1.c
+++ helper1.c
@@ -1,7 +1,11 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * find_len - finds number of digits in an integer
+ * @val: integer to find length for
+ * Return: number of digits in integer
+ */
 int find_len(int val)
 {
 	int i = 1;
